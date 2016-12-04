65f22df3d028d82e9033ae534a752d4cb02e1608
choc <choc@choc.kr>
2016-12-04 22:11:47 +0900

    first commit

---
+++ src/main/java/kr/choc/cgg/CampusgogoApplication.java
@@ -0,0 +1,12 @@
+package kr.choc.cgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusgogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusgogoApplication.class, args);
+	}
+}
