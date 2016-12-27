5c4a22508afbe75c259987bbf05a46d9e91a90cb
choc <choc@choc.kr>
2016-12-27 16:08:16 +0900

    rocket version update

--- src/main/java/kr/choc/cgg/model/rep/CorrectAnswerRepository.java
+++ src/main/java/kr/choc/cgg/model/rep/CorrectAnswerRepository.java
@@ -11,7 +11,7 @@ import kr.choc.cgg.model.CorrectAnswer;
 public interface CorrectAnswerRepository extends JpaRepository<CorrectAnswer, Long>{
 	CorrectAnswer findByNumberAndMockTestId(Integer number, Long MockTestId);
 	
-	@Query("select sum(c.score) as score from correct_answer c, mock_test m where m.year=:year and m.grade=:grade and m.month=:month and c.mock_test_id=m.id and c.number in (:faults)")
-	CorrectAnswer findScore(@Param("year") Integer year, @Param("grade") Integer grade, @Param("year") Integer month, @Param("faults") List<Integer> faults); 
+//	@Query("select sum(c.score) as score from correct_answer c, mock_test m where m.year=:year and m.grade=:grade and m.month=:month and c.mock_test_id=m.id and c.number in (:faults)")
+//	CorrectAnswer findScore(@Param("year") Integer year, @Param("grade") Integer grade, @Param("year") Integer month, @Param("faults") List<Integer> faults); 
 
 }
